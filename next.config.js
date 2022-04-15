f9b39254f03326b148bf4503d5a1a56411848c3f
carlosg <gtrujilloca@gmail.com>
2022-04-15 18:01:13 -0500

    Update PWA

--- next.config.js
+++ next.config.js
@@ -1,9 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const withPWA = require('next-pwa');
+const runtimeCaching = require('next-pwa/cache');
+
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+    register: true,
+    mode: 'production',
+    disable: false,
+    runtimeCaching,
+    buildExcludes: [/midleware-manifest.json$/]
+  },
   reactStrictMode: true,
   images: {
     domains: ['placeimg.com', 'api.lorem.space'],
   },
-};
-
-module.exports = nextConfig;
+});
